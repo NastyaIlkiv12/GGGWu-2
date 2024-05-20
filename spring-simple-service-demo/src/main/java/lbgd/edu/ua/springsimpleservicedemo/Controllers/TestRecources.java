b92de5a8128b66deef630d2f8eadc37113f5c232
Анастасия <embipomn2002@gmail.com>
2024-05-20 15:31:19 +0300

    Commites

---
+++ spring-simple-service-demo/src/main/java/lbgd/edu/ua/springsimpleservicedemo/Controllers/TestRecources.java
@@ -0,0 +1,12 @@
+package lbgd.edu.ua.springsimpleservicedemo.Controllers;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestRecources {
+
+    @GetMapping("recources/{name}")
+    public String getName(@PathVariable String name) {return "Hello"+name;}
+}
